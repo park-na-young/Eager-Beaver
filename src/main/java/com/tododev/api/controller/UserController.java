d89b4477094dc78205a9ef86c345bf897af8786f
parknayoung <na311519@gmail.com>
2024-12-16 16:50:29 +0900

    swagger 세팅 추가
    - 의존성 추가
    - UserController 어노테이션 추가
    - swaggerConfig 클래스 추가

--- src/main/java/com/tododev/api/controller/UserController.java
+++ src/main/java/com/tododev/api/controller/UserController.java
@@ -2,6 +2,8 @@ package com.tododev.api.controller;
 
 import com.tododev.api.dto.*;
 import com.tododev.api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
@@ -12,6 +14,7 @@ import org.springframework.web.context.request.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Tag(name = "User Authentication", description = "사용자 인증 및 계정 관리 API")
 public class UserController {
 
     private final UserService userService;
@@ -23,6 +26,10 @@ public class UserController {
      * @param requestBody
      * @return
      */
+    @Operation(
+            summary = "일반 로그인",
+            description = "사용자의 이메일과 비밀번호를 통해 로그인을 수행합니다."
+    )
     @PostMapping("/sign-in")
     public ResponseEntity<ApiResponseDto> signIn(
             WebRequest webRequest,
@@ -39,6 +46,10 @@ public class UserController {
      * @param requestBody
      * @return
      */
+    @Operation(
+            summary = "회원가입",
+            description = "사용자의 정보를 입력받아 새로운 계정을 생성합니다."
+    )
     @PostMapping(value = "/sign-up")
     public ResponseEntity<ApiResponseDto> signUp(
             WebRequest webRequest
